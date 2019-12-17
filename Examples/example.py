fec17be77c51293b3e16035b4dda5ea76af75087
Abdul Qadir Ibrahim <abdulqadr.ibrahim@stud.uni-goettingen.de>
2019-12-17 18:35:50 +0100

    FEM enrichment

--- Examples/example.py
+++ Examples/example.py
@@ -5,7 +5,7 @@ from netgen import gui
 
 beta = (2, 1)
 mesh_size = [1.0, 0.5, 0.25, 0.1250, 0.0625, 0.0313]
-mesh_size = [0.0625]
+#mesh_size = [0.0625]
 
 eps = 0.01
 
@@ -29,22 +29,22 @@ for size in mesh_size:
     mesh = Mesh(unit_square.GenerateMesh(maxh=size))
 
     V = L2(mesh, order=2)
-    u, v = V.TnT()
-
-    a = BilinearForm(V)
-    # a += SymbolicBFI((eps * grad(u) * grad(v)) + (b * grad(u) * v))
-    a += u * v * dx
-
-    f = LinearForm(V)
-    # f += SymbolicLFI(coeff * v)
-    f += coeff * v * dx
-
-    f.Assemble()
-    a.Assemble()
-
-    gu = GridFunction(V)
-    gu.vec.data = a.mat.Inverse(V.FreeDofs(), inverse='sparsecholesky') * f.vec
-    Draw(gu, mesh, 'test')
+    # u, v = V.TnT()
+    #
+    # a = BilinearForm(V)
+    # # a += SymbolicBFI((eps * grad(u) * grad(v)) + (b * grad(u) * v))
+    # a += u * v * dx
+    #
+    # f = LinearForm(V)
+    # # f += SymbolicLFI(coeff * v)
+    # f += coeff * v * dx
+    #
+    # f.Assemble()
+    # a.Assemble()
+    #
+    # gu = GridFunction(V)
+    # gu.vec.data = a.mat.Inverse(V.FreeDofs(), inverse='sparsecholesky') * f.vec
+    # Draw(gu, mesh, 'test')
 
     Q = L2(mesh, order=0)
     ba = BitArray(mesh.ne)
@@ -58,11 +58,11 @@ for size in mesh_size:
     p = coeff * p
     q = coeff * q
 
+    grad_u = grad(u) + CoefficientFunction((coeff.Diff(x), coeff.Diff(y))) * p
+    grad_v = grad(v) + CoefficientFunction((coeff.Diff(x), coeff.Diff(y))) * q
+
     u = u + p
     v = v + q
-
-    # grad_u = grad(u) + CoefficientFunction((coeff.Diff(x), coeff.Diff(y))) * p
-    # grad_v = grad(v) + CoefficientFunction((coeff.Diff(x), coeff.Diff(y))) * q
 
     a = BilinearForm(W)
     # a += SymbolicBFI((eps * grad_u * grad_v) + (b * grad_u * v))
@@ -79,5 +79,8 @@ for size in mesh_size:
     gu2.vec.data = a.mat.Inverse(
         W.FreeDofs(), inverse='sparsecholesky') * f.vec
     u2 = gu2.components[0] + gu2.components[1] * coeff
-    Draw(u2, mesh, 'test2')
-    input('running everything')
+    print('Mesh Size:', size, "L2 Error:", sqrt(
+        Integrate((u2 - exact)*(u2 - exact), mesh)))
+
+    #Draw(u2, mesh, 'test2')
+    # input('running everything')
