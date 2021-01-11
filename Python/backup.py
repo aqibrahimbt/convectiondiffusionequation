0a85aae056985d156d490275834f2e9607c16c48
Abdul Qadir Ibrahim <abdulqadir.ibrahim>
2021-01-11 14:59:28 +0100

    computing trace inverse inequality

--- Python/backup.py
+++ Python/backup.py
@@ -52,6 +52,47 @@ class Convection_Diffusion():
 
                         if len(self.config['enrich_functions']) > 0:
                             type = str('edg')
+                        # # Checking linear dependence
+                        #     ipintegrator = SymbolicBFI(
+                        #         u() * v(), bonus_intorder=bonus_int)
+                        #     ba_active_elements = BitArray(mesh.ne)
+                        #     for enr_indicator in self.config['enrich_domain_ind']:
+                        #         ba_active_elements |= mark_elements(
+                        #             mesh, enr_indicator, size)
+
+                        #     for el in fes.Elements():
+                        #         if ba_active_elements[el.nr]:
+                        #             i = ElementId(el)
+                        #             N = len(el.dofs)
+                        #             element = fes.GetFE(el)
+                        #             elementstd = V.GetFE(i)
+                        #             Nstd = elementstd.ndof
+                        #             trafo = mesh.GetTrafo(i)
+                        #             # Get element matrix
+                        #             elmat = ipintegrator.CalcElementMatrix(
+                        #                 element, trafo)
+                        #             important = [True if el.dofs[i] >=
+                        #                         0 else False for i in range(N)]
+                        #             # before_important = [
+                        #             #     True if el.dofs[i] >= 0 else False for i in range(N)]
+
+                        #             factors = []
+                        #             for i in range(Nstd, N):
+                        #                 if important[i]:
+                        #                     active = [j for j in range(
+                        #                         i) if important[j]]
+                        #                     factor = 1 - 2 * \
+                        #                         sum([elmat[i, j]**2/elmat[i, i] /
+                        #                             elmat[j, j] for j in active])
+                        #                     factor += sum([elmat[i, j]*elmat[i, k]*elmat[j, k]/elmat[i, i] /
+                        #                                 elmat[j, j]/elmat[k, k] for j in active for k in active])
+                        #                     factor = sqrt(abs(factor))
+                        #                     factors.append(factor)
+                        #                     if (factor <= 1e-3):
+                        #                         important[i] = False
+                        #                         if el.dofs[i] >= 0:
+                        #                             ba_active_dofs[el.dofs[i]
+                        #                                         ] = False
                         else:
                             type = str('dg')
                         
