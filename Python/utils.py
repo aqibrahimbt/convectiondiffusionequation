14c35395845ed1b3d2eadf97e04da61e70fe0926
Abdul Qadir Ibrahim <abdulqadir.ibrahim>
2021-02-10 00:40:18 +0100

    code cleanup

--- Python/utils.py
+++ Python/utils.py
@@ -1,32 +1,5 @@
-import matplotlib.image as mpimg
-from IPython.display import Image, display
-from functools import reduce
-import pandas as pd
 import matplotlib.pyplot as plt
 plt.style.use('seaborn-white')
-
-# TODO: Modularize
-
-
-# def plot_error(table):
-#     order_1 = table[table.Order.eq(1.0)]
-#     order_2 = table[table.Order.eq(2.0)]
-#     order_3 = table[table.Order.eq(3.0)]
-#     order_4 = table[table.Order.eq(4.0)]
-
-#     fig, (ax1, ax2, ax3, ax4) = plt.subplots(1, 4, figsize=(20, 5))
-
-#     order_1.plot(x='Mesh Size', y='Error', ax=ax1, legend=True,
-#                  title='k = 1', style='.-', loglog=True)
-#     order_2.plot(x='Mesh Size', y='Error', ax=ax2, legend=True,
-#                  title='k = 2', style='.-', loglog=True)
-#     order_3.plot(x='Mesh Size', y='Error', ax=ax3, legend=True,
-#                  title='k = 3', style='.-', loglog=True)
-#     order_4.plot(x='Mesh Size', y='Error', ax=ax4, legend=True,
-#                  title='k = 4', style='.-', loglog=True)
-
-#     plt.show()
-
 
 def plot_error_dof(table):
     order_1 = table[table.Order.eq(1.0)]
